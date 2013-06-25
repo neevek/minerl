2af0e54e9e585d1e0c73db99c088243ed865e5ac
neevek <i@neevek.net>
2013-06-25 22:00:56 +0800

    replace Text::Markdown with Text::MultiMarkdown.

--- lib/Minerl/Formatter/Markdown.pm
+++ lib/Minerl/Formatter/Markdown.pm
@@ -5,6 +5,7 @@ use 5.10.0;
 package Minerl::Formatter::Markdown;
 
 my $instance;
+my $markdownInstance;
 
 sub new {
     my $class = shift;
@@ -12,19 +13,25 @@ sub new {
     if (!$instance) {
         $instance = bless {}, $class;
 
-        my $useStr = "use Text::Markdown;";
+        my $useStr = "use Text::MultiMarkdown;";
         eval($useStr);
         $instance->{"available"} = !$@;
+
+        $markdownInstance = Text::MultiMarkdown->new(
+            empty_element_suffix => '>',
+            tab_width => 2,
+            use_wikilinks => 1,
+        ) if !$@;
     }
 
-    warn "Warning: Text::Markdown is not installed, Markdown text will not be parsed." if !$instance->{"available"};
+    warn "Warning: Text::MultiMarkdown is not installed, Markdown text will not be parsed." if !$instance->{"available"};
 
     return $instance;
 }
 
 sub format { 
     my ($self, $content) = @_;
-    return $self->{"available"} ? Text::Markdown::markdown($$content) : $$content;
+    return $markdownInstance ? $markdownInstance->markdown($$content) : $$content;
 }
 
 1;
--- pages/index.md
+++ pages/index.md
@@ -15,3 +15,11 @@ Hello! This is **Neevek**, I just wrote this static site generator - **Minerl**.
 - many others for you to explore
 
 go get it [here](https://github.com/neevek).
+
+Let's write some code:
+
+    public static void main (String args []) {
+        System.out.println("hello world"); 
+    }
+
+This is `inline` code.
